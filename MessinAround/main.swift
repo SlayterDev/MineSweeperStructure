08afc7ebee9ae075864dcb477394b9897226a078
Bradley Slayter <bslayter@axxess.com>
2016-03-29 13:53:47 -0500

    Initial Commit

---
+++ MessinAround/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MessinAround
+//
+//  Created by Bradley Slayter on 3/29/16.
+//  Copyright © 2016 Slayter Development. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
